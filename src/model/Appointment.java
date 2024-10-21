6f1ca68ef4250c09705d80cfab2d30998edb8a37
WRT4 <165753918+WRT4@users.noreply.github.com>
2024-10-21 21:26:08 +0800

    Update Appointment.java

--- src/model/Appointment.java
+++ src/model/Appointment.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Appointment {
     private Patient patient;
     private Doctor doctor;
@@ -7,10 +10,10 @@ public class Appointment {
     private String time;
     private String status;
     private String prescriptionStatus;
-    private String AppointmenID;
+    private String AppointmentID;
     private static int count;
     private String prescription;
-
+    
     public Appointment(Patient patient, Doctor doctor, String date, String time) {
         this.patient = patient;
         this.doctor = doctor;
@@ -18,7 +21,7 @@ public class Appointment {
         this.time = time;
         this.status = "PENDING";
         count++;
-        this.AppointmenID = "" + count;
+        this.AppointmentID = "" + count;
     }
 
     public String getDate() {
@@ -61,13 +64,13 @@ public class Appointment {
     }
 
     //print Appointment Outcome Record (for COMPLETED appointments)
-    public void printAppointmentOutcome(int appointmentNum){
-        System.out.println("Appointment " + appointmentNum + ": " + date + " at " + time + " with Doctor " + doctor.getName()); //getDoctorName
+    public void printAppointmentOutcome(){
+        System.out.println("Appointment " + AppointmentID + ": " + date + " at " + time + " with Doctor " + doctor.getName()); //getDoctorName
     }
 
 	public String getAppointmentID() {
 		// TODO Auto-generated method stub
-		return this.AppointmenID;
+		return this.AppointmentID;
 	}
 
 	public String getPrescription() {
@@ -82,6 +85,79 @@ public class Appointment {
 		return doctor;	
 	}
 
-
+	public static String inputDate() {
+		Scanner sc = new Scanner(System.in);
+		int day=0, month=0;
+		while (true) {
+			try {
+				System.out.println("Enter Day: ");
+				day = sc.nextInt();
+				if (day > 31 || day < 1) {
+					throw new RuntimeException("Invalid input for Day!");
+				}
+				System.out.println("Enter Month: ");
+				month = sc.nextInt();
+				if (month > 12 || month < 1) {
+					throw new RuntimeException("Invalid input for Month!");
+				}
+				break;
+			}
+			catch (InputMismatchException e) {
+				System.out.println("Wrong input type! Try Again!");
+				sc.nextLine();
+				continue;
+			}
+			catch (RuntimeException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+				continue;
+			}
+			catch (Exception e) {
+				System.out.println("Error! Try Again!");
+				sc.nextLine();
+				continue;
+			}
+		}
+		sc.close();
+		return "2024-" + String.format("%02d", month) + "-" + String.format("%02d", day);
+	}
+	
+	public static String inputTime() {
+		Scanner sc = new Scanner(System.in);
+		int hour=0, min=0;
+		while (true) {
+			try {
+				System.out.println("Enter Hour (24 hour format): ");
+				hour = sc.nextInt();
+				if (hour > 23 || hour < 0) {
+					throw new RuntimeException("Invalid input for Hour!");
+				}
+				System.out.println("Enter Minute: ");
+				min = sc.nextInt();
+				if (min > 59 || min < 0) {
+					throw new RuntimeException("Invalid input for Minute!");
+				}
+				break;
+			}
+			catch (InputMismatchException e) {
+				System.out.println("Wrong input type! Try Again!");
+				sc.nextLine();
+				continue;
+			}
+			catch (RuntimeException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+				continue;
+			}
+			catch (Exception e) {
+				System.out.println("Error! Try Again!");
+				sc.nextLine();
+				continue;
+			}
+		}
+		sc.close();
+		return String.format("%02d", hour) + ":" + String.format("%02d", min);
+	}
+	
 
 }
