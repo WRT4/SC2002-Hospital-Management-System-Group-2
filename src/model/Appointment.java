3f06a8516f9a205dd7463fc52d518b0742451574
WRT4 <165753918+WRT4@users.noreply.github.com>
2024-10-21 16:03:13 +0800

    Update Appointment.java

--- src/model/Appointment.java
+++ src/model/Appointment.java
@@ -1,19 +1,24 @@
 package model;
 
 public class Appointment {
-    private String patientID;
-    private String doctorID;
+    private Patient patient;
+    private Doctor doctor;
     private String date;
     private String time;
     private String status;
     private String prescriptionStatus;
+    private String AppointmenID;
+    private static int count;
+    private String prescription;
 
-    public Appointment(String patientID, String doctorID, String date, String time) {
-        this.patientID = patientID;
-        this.doctorID = doctorID;
+    public Appointment(Patient patient, Doctor doctor, String date, String time) {
+        this.patient = patient;
+        this.doctor = doctor;
         this.date = date;
         this.time = time;
         this.status = "PENDING";
+        count++;
+        this.AppointmenID = "" + count;
     }
 
     public String getDate() {
@@ -51,12 +56,25 @@ public class Appointment {
     //to confirm with "Schedule" entity
     public void printScheduledAppointment(){
         System.out.println("Appointment: " + date + " at " + time +
-                "\n with Doctor: " + doctorID);//getName for doctor
+                "\n with Doctor: " + doctor.getName() );//getName for doctor
+        System.out.println("Status: " + status);
     }
 
     //print Appointment Outcome Record (for COMPLETED appointments)
     public void printAppointmentOutcome(int appointmentNum){
-        System.out.println("Appointment " + appointmentNum + ": " + date + " at " + time + " with Doctor " + ); //getDoctorName
+        System.out.println("Appointment " + appointmentNum + ": " + date + " at " + time + " with Doctor " + doctor.getName()); //getDoctorName
     }
 
+	public String getAppointmentID() {
+		// TODO Auto-generated method stub
+		return this.AppointmenID;
+	}
+
+	public String getPrescription() {
+		return this.prescription;
+	}
+	
+	public void setPrescription(String prescription) {
+		this.prescription = prescription;
+	}
 }
