4125807ddaab3bf10da7de1e132cec5b9ff0e142
WRT4 <165753918+WRT4@users.noreply.github.com>
2024-10-26 04:13:21 +0800

    Add files via upload

---
+++ src/model/Database.java
@@ -0,0 +1,9 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Database {
+	public static final ArrayList<Doctor> doctors = new ArrayList<Doctor>();
+	public static final ArrayList<Patient> patients = new ArrayList<Patient>();
+	
+}
