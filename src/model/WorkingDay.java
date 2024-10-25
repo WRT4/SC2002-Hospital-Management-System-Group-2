e03f4b1dbfe1f7e610281fdeabe897fa5c08fc2a
WRT4 <165753918+WRT4@users.noreply.github.com>
2024-10-25 12:16:03 +0800

    Add files via upload

--- src/model/WorkingDay.java
+++ src/model/WorkingDay.java
@@ -10,6 +10,15 @@ public class WorkingDay {
         //create 15-min time slots
         workingSlot = new ArrayList<>();
         createWorkingTimeSlot(workStartTime, workEndTime);
+    }public class WorkingDay {
+    private String dayName; 
+    private ArrayList<TimeSlot> workingSlot; 
+
+    public WorkingDay(String dayName, LocalTime workStartTime, LocalTime workEndTime){
+        this.dayName = dayName; 
+        //create 15-min time slots
+        workingSlot = new ArrayList<>();
+        createWorkingTimeSlot(workStartTime, workEndTime);
     }
 
     public void createWorkingTimeSlot(LocalTime workStartTime, LocalTime workEndTime){
