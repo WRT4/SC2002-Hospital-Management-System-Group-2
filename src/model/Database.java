0a2b6d959e20eddef9f8fbae65ef4e0c57964ca5
WRT4 <165753918+WRT4@users.noreply.github.com>
2024-11-03 15:42:29 +0800

    Delete src/model directory

--- src/model/Database.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-import java.util.ArrayList;
-
-public class Database {
-	public static final ArrayList<Doctor> doctors = new ArrayList<Doctor>();
-	public static final ArrayList<Patient> patients = new ArrayList<Patient>();
-	
-}
