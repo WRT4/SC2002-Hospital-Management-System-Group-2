068eb3fe870e37fb2221f052fdd023ed1db92cf5
TheWikiZ <157239810+TheWikiZ@users.noreply.github.com>
2024-11-08 17:11:12 +0800

    Update Database.java

--- temp/Database.java
+++ temp/Database.java
@@ -6,5 +6,15 @@ import java.util.LinkedList;
 public class Database {
 	public static final ArrayList<Doctor> doctors = new ArrayList<Doctor>();
 	public static final ArrayList<Patient> patients = new ArrayList<Patient>();
+	public static final ArrayList<Pharmacist> pharmacists = new ArrayList<Pharmacist>();
+	public static final ArrayList<Administrator> administrators = new ArrayList<Administrator>();
 	
+	public static final MedicationBank medicationBank = new MedicationBank();
+	
+	public static int getTotalStaffCount() {
+	        return doctors.size() + pharmacists.size() + administrators.size();
+	    }
 }
+
+
+
