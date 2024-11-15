fe4f1f9fbada778f5738ce0fc1805ed81b6d276f
Wen Rong <165753918+WRT4@users.noreply.github.com>
2024-11-15 13:31:41 +0800

    Add files via upload

--- src/application/HospitalManagementApp.java
+++ src/application/HospitalManagementApp.java
@@ -10,33 +10,40 @@ public class HospitalManagementApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Database db = new Database();
-        User user;
+        User user = null;
 
         // Main loop for the application
+//        while (true) {
+//            // Prompt the user for their User ID
+//            user = UserView.getUser(scanner);
+//            if (user == null) {
+//            	System.out.println("Exiting the application!");
+//            	return;
+//            }
+//            
+//            // Prompt the user for their password
+//            System.out.print("Enter Password: ");
+//            String inputPassword = scanner.nextLine();
+//            
+//            UserController userController = new UserController(user,scanner);
+//            
+//            // Delegate login handling to the User class
+//            boolean loggedIn = userController.login(user.getID(), inputPassword);
+//
+//            // If login is successful, access the user's menu
+//            if (loggedIn) {
+//                userController.showMenu();
+//            }
+//
+//            // After the user logs out, return to the login prompt
+//            System.out.println("You have logged out. Returning to the login page...");
+//        }
         while (true) {
-            // Prompt the user for their User ID
-            user = UserView.getUser(scanner);
-            if (user == null) {
-            	System.out.println("Exiting the application!");
-            	return;
-            }
-            
-            // Prompt the user for their password
-            System.out.print("Enter Password: ");
-            String inputPassword = scanner.nextLine();
-            
-            UserController userController = new UserController(user,scanner);
-            
-            // Delegate login handling to the User class
-            boolean loggedIn = userController.login(user.getID(), inputPassword);
-
-            // If login is successful, access the user's menu
-            if (loggedIn) {
-                userController.showMenu();
-            }
-
-            // After the user logs out, return to the login prompt
-            System.out.println("You have logged out. Returning to the login page...");
+        	UserController userController = new UserController(user,scanner);
+        	userController.showMenu();
+        	
+        	// After the user logs out, return to the login prompt
+        	System.out.println("You have logged out. Returning to the login page...");
         }
     }
 }
