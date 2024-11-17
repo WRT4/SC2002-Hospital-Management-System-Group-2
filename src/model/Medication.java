b0b20156cf6e7f6ad6828d1bffee5b95f2f4bd09
jingh <jinghuanhoo@gmail.com>
2024-11-17 23:31:27 +0800

    removed medication expiry feature

--- src/model/Medication.java
+++ src/model/Medication.java
@@ -6,14 +6,12 @@ public class Medication {
     private String name;
     private int stockLevel;
     private int dosage;
-    private LocalDate expirationDate;
     private int lowStockThreshold;
 
     public Medication(String name, int stockLevel,  LocalDate expirationDate, int lowStockThreshold) {
         this.name = name;
         this.stockLevel = stockLevel;
         //this.dosage = dosage;
-        this.expirationDate = expirationDate;
         this.lowStockThreshold = lowStockThreshold;
     }
     
@@ -35,24 +33,12 @@ public class Medication {
         return dosage;
     }
 
-    public LocalDate getExpirationDate() {
-        return expirationDate;
-    }
-
     public int getLowStockThreshold() {
         return lowStockThreshold;
     }
 
     public void setStockLevel(int stockLevel) {
         this.stockLevel = stockLevel;
-    }
-
-    public void setExpirationDate(LocalDate expirationDate) {
-        this.expirationDate = expirationDate;
-    }
-
-    public boolean isExpired() {
-        return LocalDate.now().isAfter(expirationDate);
     }
 
     public boolean isStockLow() {
@@ -98,14 +84,12 @@ public class Medication {
         }
 
         // Include additional fields only if they are initialized (used in the first constructor)
-        if (stockLevel > 0 || expirationDate != null || lowStockThreshold > 0) {
+        if (stockLevel > 0 || lowStockThreshold > 0) {
             result.append(", Stock Level: ").append(stockLevel)
-                  .append(", Expiration Date: ").append(expirationDate != null ? expirationDate : "N/A")
                   .append(", Low Stock Threshold: ").append(lowStockThreshold);
 
             // Add status if stock is low or expired
             if (isStockLow()) result.append(" (Low Stock)");
-            if (isExpired()) result.append(" (Expired)");
         }
 
         return result.toString();
